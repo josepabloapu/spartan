a3bf2504a3950913fd7ddc74036e1a4c30ab71c2
Jose Pablo Apu Picado <josepabloapu@gmail.com>
2015-08-26 08:09:25 -0600

    src del lab 2

---
+++ lab2/src/MiniAlu.v
@@ -0,0 +1,215 @@
+
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+module MiniAlu
+(
+ input wire Clock,
+ input wire Reset,
+ output wire [7:0] oLed
+
+);
+
+wire [15:0]	wIP,wIP_temp;
+reg        	rWriteEnable,rBranchTaken;
+wire [27:0] wInstruction;
+wire [3:0]  wOperation;
+reg [15:0]  rResult;
+wire [7:0]  wSourceAddr0,wSourceAddr1,wDestination,wDestination_pre;
+wire [15:0] wSourceData0,wSourceData1,wIPInitialValue,wImmediateValue,wResult_pre,wSourceData0_FromRam,wSourceData1_FromRam;
+
+ROM InstructionRom 
+(
+	.iAddress(     wIP          ),
+	.oInstruction( wInstruction )
+);
+
+RAM_DUAL_READ_PORT DataRam
+(
+	.Clock(         Clock        ),
+	.iWriteEnable(  rWriteEnable ),
+	.iReadAddress0( wInstruction[7:0] ),
+	.iReadAddress1( wInstruction[15:8] ),
+	.iWriteAddress( wDestination ),
+	.iDataIn(       rResult      ),
+	.oDataOut0(     wSourceData0_FromRam ),
+	.oDataOut1(     wSourceData1_FromRam )
+);
+
+assign wIPInitialValue = (Reset) ? 8'b0 : wDestination;
+UPCOUNTER_POSEDGE IP
+(
+.Clock(   Clock                ), 
+.Reset(   Reset | rBranchTaken ),
+.Initial( wIPInitialValue + 1  ),
+.Enable(  1'b1                 ),
+.Q(       wIP_temp             )
+);
+assign wIP = (rBranchTaken) ? wIPInitialValue : wIP_temp;
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD1 
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[27:24]),
+	.Q(wOperation)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD2
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[7:0]),
+	.Q(wSourceAddr0)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD3
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[15:8]),
+	.Q(wSourceAddr1)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD4
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[23:16]),
+	.Q(wDestination)
+);
+
+
+reg rFFLedEN;
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FF_LEDS
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable( rFFLedEN ),
+	.D( wSourceData1 ),
+	.Q( oLed    )
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FF_DESTINATION
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable( 1'b1 ),
+	.D( wDestination ),
+	.Q( wDestination_pre)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 16 ) FF_RESULT
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable( 1'b1 ),
+	.D( rResult ),
+	.Q( wResult_pre)
+);
+
+assign wImmediateValue = {wSourceAddr1,wSourceAddr0};
+
+
+// Corrección del hazard de RaW
+// Se utiliza un comparador y se verifica si algun SourceAddr es igual al DestinarionAddr anterior
+// Si es igual permite que el source tome el resultado más rápido del destino
+assign wSourceData0 = ( wSourceAddr0 == wDestination_pre) ? wResult_pre :  wSourceData0_FromRam;
+assign wSourceData1 = ( wSourceAddr1 == wDestination_pre) ? wResult_pre : wSourceData1_FromRam;
+
+// Descomentar para observar el pipeline con el harzard de RaW
+//assign wSourceData0 = wSourceData0_FromRam;
+//assign wSourceData1 = wSourceData1_FromRam;
+
+always @ ( * )
+begin
+	case (wOperation)
+	//-------------------------------------
+	`NOP:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+	end
+	//-------------------------------------
+	`ADD:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rResult      <= wSourceData1 + wSourceData0;
+	end
+	//-------------------------------------
+	// Se agregó la función SUB (resta)
+	`SUB:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b1;
+		// Es posible hacer rResult <= SourceData1 + SourceData0 "en complemento a 2"
+		rResult      <= wSourceData1 - wSourceData0;
+	end
+	//-------------------------------------
+	// Se agregó la función MUL (multiplicación)
+	`MUL:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rResult      <= wSourceData1 * wSourceData0;
+	end
+	//-------------------------------------
+	`STO:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rBranchTaken <= 1'b0;
+		rResult      <= wImmediateValue;
+	end
+	//-------------------------------------
+	`BLE:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		if (wSourceData1 <= wSourceData0 )
+			rBranchTaken <= 1'b1;
+		else
+			rBranchTaken <= 1'b0;
+		
+	end
+	//-------------------------------------	
+	`JMP:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b1;
+	end
+	//-------------------------------------	
+	`LED:
+	begin
+		rFFLedEN     <= 1'b1;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b0;
+	end
+	//-------------------------------------
+	default:
+	begin
+		rFFLedEN     <= 1'b1;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b0;
+	end	
+	//-------------------------------------	
+	endcase	
+end
+
+
+endmodule
---
+++ lab2/src/Module_ROM (original).v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+`define LOOP1 8'd8
+`define LOOP2 8'd5
+module ROM
+(
+	input  wire[15:0]  		iAddress,
+	output reg [27:0] 		oInstruction
+);	
+always @ ( iAddress )
+begin
+	case (iAddress)
+
+	0: oInstruction = { `NOP ,24'd4000    };
+	// Inicialización de registros
+	1: oInstruction = { `STO , `R7,16'b0001 };
+	2: oInstruction = { `STO ,`R3,16'h1     }; 
+	3: oInstruction = { `STO, `R4,16'd1000 };
+	4: oInstruction = { `STO, `R5,16'd0     };  //j
+//LOOP2:
+	// Led toma el valor de R7
+	5: oInstruction = { `LED ,8'b0,`R7,8'b0 };
+	6: oInstruction = { `STO ,`R1,16'h0     };
+	// Al cambiar 65000 por un valor menor se aumento la velocida del contador	 	
+	// Linea original: -- 7: oInstruction = { `STO ,`R2,16'd65000 };
+	7: oInstruction = { `STO ,`R2,16'd500 };
+//LOOP1:	
+	8: oInstruction = { `ADD ,`R1,`R1,`R3    }; 
+	9: oInstruction = { `BLE ,`LOOP1,`R1,`R2 }; 
+	
+	10: oInstruction = { `ADD ,`R5,`R5,`R3    };
+	11: oInstruction = { `BLE ,`LOOP2,`R5,`R4 };	
+	12: oInstruction = { `NOP ,24'd4000       };
+	// Se le va sumando a R7 una unida cada ciclo (R7++) 
+	13: oInstruction = { `ADD ,`R7,`R7,`R3    };
+	14: oInstruction = { `JMP ,  8'd2,16'b0   };
+	default:
+		oInstruction = { `LED ,  24'b10101010 };		//NOP
+	endcase	
+end
+	
+endmodule
