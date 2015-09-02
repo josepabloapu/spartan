d109f6d2f90f2bdf6f7a64272b12e8cdaf30243f
Guengo <guengo@ubuntu.ubuntu-domain>
2015-09-02 08:35:55 -0600

    se agrega multiplicador 4 bits

--- lab2/src/mul.v
+++ lab2/src/mul.v
@@ -45,7 +45,38 @@ assign ResultMul[2] = {C1^A[1]&B[1]^A[2]&B[0]^A[0]&B[2]  };
 assign C2 = {C1&A[1]&B[0]|C1&A[0]&B[1]|A[1]&B[0]&A[0]&B[1]};
 
 
-assign ResultMul[2] = A[2]&B[0]+A[1]&B[1]+ A[0]&B[2]};
+assign ResultMul[2] = {A[2]&B[0]+A[1]&B[1]+ A[0]&B[2]};
 
 
 endmodule
+//////////////////////////////////
+module mul_cuatro_bits(
+input wire [3:0] wA,
+input wire [3:0] wB,
+output [7:0] out
+);
+reg wCA0,wCA1,wCA2,wCA3;
+reg wOutA0,wOutA1,wOutA2,wOutA3;
+reg wCB0,wCB1,wCB2,wCB3;
+reg wOutB0,wOutB1,wOutB2,wOutB3;
+reg wCC0,wCC1,wCC2,wCC3;
+reg wOutC0,wOutC1,wOutC2,wOutC3;
+
+
+//Nivel A
+{wCA0,wOutA0}=(wA[0]&wB[1])+(wA[1]&wB[0]);
+{wCA1,wOutA1}<=(wA[2]&wB[0])+(wA[1]&wB[1])+wCA0;
+{wCA2,wOutA2}<=(wA[3]&wB[0])+(wA[2]&wB[1])+wCA1;
+{wCA3,wOutA3}<=(wA[3]&wB[1])+wCA2;
+//NIvel B
+{wCB0,wOutB0}<=wOutA1+(wA[0]&wB[2]);
+{wCB1,wOutB1}<=wOutA2+(wA[1]&wB[2])+wCB0;
+{wCB2,wOutB2}<=wOutA3+(wA[2]&wB[2])+wCB1;
+{wCB3,wOutB3}<=wCA3+(wA[3]&wB[2])+wCB2;
+//Nivel C
+{wCC0,wOutC0}<=wOutB1+(wA[0]&wB[3]);
+{wCC1,wOutC1}<=wOutB2+(wA[1]&wB[3])+wCC0;
+{wCC2,wOutC2}<=wOutB3+(wA[2]&wB[3])+wCC1;
+{wCC3,wOutC3}<=wCB3+(wA[3]&wB[3])+wCC2;
+out<={wCC3,wOutC3,wOutC2,wOutC1,wOutC0,wOutB0,wOutA0,(wA[0]&wB[0])};
+endmodule		
