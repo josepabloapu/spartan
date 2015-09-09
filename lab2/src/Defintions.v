a9c9c04edee5dcdcca90c00b0099daa2d7fec69b
Jose Pablo Apu Picado <josepabloapu@gmail.com>
2015-09-09 07:44:24 -0600

    vaca

--- lab2/src/Defintions.v
+++ lab2/src/Defintions.v
@@ -10,8 +10,9 @@
 `define ADD   4'd5
 `define SUB   4'd6
 `define MUL   4'd7
-`define IMUL   4'd8
-`define JMP   4'd9
+`define IMUL  4'd8
+`define SMUL  4'd9
+`define JMP   4'd10
 
 `define R0 8'd0
 `define R1 8'd1
--- script/Module_ROM.v
+++ script/Module_ROM.v
@@ -14,21 +14,22 @@ begin
 	case (iAddress)
 	
 	0: oInstruction = { `NOP ,	24'd4000	};
-	1: oInstruction = { `STO , `R1, 16'd0};
-	2: oInstruction = { `STO , `R2, 16'd65000};
-	3: oInstruction = { `STO , `R3, 16'd1};
-	4: oInstruction = { `STO , `R4, 16'd1000};
-	5: oInstruction = { `STO , `R5, 16'd0};
-	6: oInstruction = { `STO , `R6, 16'd4};
-	7: oInstruction = { `STO , `R7, 16'd5};
-	8: oInstruction = { `LED ,	8'b0, `R7, 8'b0 };
-	9: oInstruction = { `ADD , `R3, `R1, `R3};
-	10: oInstruction = { `BLE , `LOOP2, `R1, `R2};
-	11: oInstruction = { `ADD , `R5, `R5, `R3};
-	12: oInstruction = { `BLE , `LOOP1, `R1, `R2};
-	13: oInstruction = { `NOP ,	24'd4000	};
-	14: oInstruction = { `MUL , `R7, `R0, `R6 };
-	15: oInstruction = { `JMP , 8'd2, 16'b0 };
+	1: oInstruction = { `STO , `R0, 16'd2};
+	2: oInstruction = { `STO , `R6, 16'd4};
+	3: oInstruction = { `STO , `R7, 16'd1};
+	4: oInstruction = { `STO , `R3, 16'd1};
+	5: oInstruction = { `STO , `R4, 16'd1};
+	6: oInstruction = { `STO , `R5, 16'd0};
+	7: oInstruction = { `LED ,	8'b0, `R7, 8'b0 };
+	8: oInstruction = { `STO , `R1, 16'd0};
+	9: oInstruction = { `STO , `R2, 16'd6};
+	10: oInstruction = { `ADD , `R3, `R1, `R3};
+	11: oInstruction = { `BLE , `LOOP2, `R1, `R2};
+	12: oInstruction = { `ADD , `R5, `R5, `R3};
+	13: oInstruction = { `BLE , `LOOP1, `R1, `R2};
+	14: oInstruction = { `NOP ,	24'd4000	};
+	15: oInstruction = { `IMUL , `R7, `R0, `R6 };
+	16: oInstruction = { `JMP , 8'd2, 16'b0 };
 
 
 	default:
